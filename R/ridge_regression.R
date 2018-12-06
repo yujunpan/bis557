c8af8aa1e598222d4ee9eba06ade15a986f2af58
panyujun1215@gmail.com <panyujun1215@gmail.com>
2018-12-05 22:06:17 -0500

    commit

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -1,30 +1,29 @@
-#' Fit a Ridge Regression Model
+#' Fit a ridge regression model
 #'
-#' @description This function passes parameters to the ridge_reg function.
-#' @param form A formula
-#' @param lambda regularization parameter
-#' @param d A data.frame
-#' @return An list contain coefficients, lambda and formula
-#' @importFrom stats model.matrix 
-#' @examples
+#' @description This function passes parameters to the ridge regression function.
+#' @param formula a formula
+#' @param lambda a hyper parameter
+#' @param data a data.frame
+#' @return An ridge regression object
 #' @export
 
-
-ridge_reg <- function(form, lambda, d){
-  rownames(data) <- NULL
-  m <- model.matrix(form, d)
-  y <- matrix(d[,as.character(form)[2]], ncol = 1)
-  y <- y[as.numeric(rownames(m)),, drop = FALSE]
+#build ridge function
+ridge_reg<-function(formula, lambda, data){
+  rownames(data) = NULL
+  m<-model.matrix(formula, data)
+  y<-matrix(data[,as.character(formula)[2]],ncol=1)
+  y<-y[as.numeric(rownames(m)),,drop=FALSE]
   
-  svd_obj <- svd(m)
-  U <- svd_obj$u
-  V <- svd_obj$v
-  svals <- svd_obj$d
+  #Fit via svd
+  svd_obj<-svd(m)
+  U<-svd_obj$u
+  V<-svd_obj$v
+  svals<-svd_obj$d
   
-  D <- diag(svals / (svals^2 + lambda))
-  beta <- V %*% D %*% t(U) %*% y
-  rownames(beta) <- colnames(m)
-  ret <- list(coefficients = beta, lambda = lambda, form = form)
-  class(ret) <- "ridge_reg"
-  return(ret)
-}
+  D<-diag(svals/(svals^2 +lambda))
+  beta<-V %*% D %*% t(U) %*% y
+  rownames(beta) = colnames(m)
+  ret<- list(coefficients = beta, lambda=lambda,formula=formula)
+  class(ret)<-"ridge_reg"
+  ret
+}#function end
